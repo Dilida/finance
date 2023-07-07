b6a9938e3a5285fafa336d14a09eabb118190ffb
Dilida <dilida.tseng@hwaotech.com>
2023-07-07 15:19:55 +0800

    giveup context

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,6 +3,7 @@ import {Container} from "react-bootstrap";
 import DropClassMenu from "./DropClassMenu";
 import {userLoginKey, classSelectKey} from "../config";
 import {useNavigate} from 'react-router-dom';
+import Logo from '../assets/img/logo.png'
 
 const Menu = () => {
   const [mobileMenu, setMobileMenu] = useState([])
@@ -54,7 +55,15 @@ const Menu = () => {
         <div className="container d-flex align-items-center justify-content-between">
 
           <div className="logo">
-            <h1 className="text-light"><a href="/"><span><strong>e</strong> 化金檢知識網</span></a></h1>
+            <div className="row">
+              <div className="col-md-7" data-aos="fade-up" data-aos-delay="100">
+                <a href="/" title="e化金檢知識網"><img src={Logo} alt="金融監督管理委會員檢查局"/></a>
+              </div>
+              <div className="col-md-4" data-aos="fade-up" data-aos-delay="100">
+                <h1 className="text-light"><a href="/" title="e化金檢知識網"><span><strong>e</strong>化金檢知識網</span></a></h1>
+              </div>
+            </div>
+
           </div>
 
           <nav id="navbar" className={mobileMenu ? "navbar" : "navbar navbar-mobile"}>
@@ -66,11 +75,11 @@ const Menu = () => {
                 handleDropDown2={handleDropDown2}
                 handleSelect={handleSelect}
               /> : null}
-              <li><a className="nav-link scrollto" onClick={scrollHandle} id="about-">金檢學堂</a></li>
-              <li><a className="nav-link scrollto" onClick={scrollHandle} id="hero-">學習地圖</a></li>
-              <li><a className="nav-link scrollto" onClick={scrollHandle} id="contact-">意見區</a></li>
-              <li><a className="nav-link scrollto" href="/suggestion">評價結果</a></li>
-              {userLogin ? null:  <li><a className="getstarted scrollto" onClick={scrollHandle} id="services-">進入課程</a></li>}
+              <li><a className="nav-link scrollto" onClick={scrollHandle} id="about-" title="金檢學堂">金檢學堂</a></li>
+              <li><a className="nav-link scrollto" onClick={scrollHandle} id="hero-" title="學習地圖">學習地圖</a></li>
+              <li><a className="nav-link scrollto" onClick={scrollHandle} id="contact-" title="意見區">意見區</a></li>
+              <li><a className="nav-link scrollto" href="/suggestion" title="評價結果">評價結果</a></li>
+              {userLogin ? null:  <li><a className="getstarted scrollto" onClick={scrollHandle} id="services-" title="進入課程">進入課程</a></li>}
 
             </ul>
             <i className={mobileMenu ? "bi bi-list mobile-nav-toggle" : "bi mobile-nav-toggle bi-x"}
