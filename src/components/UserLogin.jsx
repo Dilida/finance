e4423f32c40d5bb2f884ebede2ad6cb2a0db1c06
Dilida <dilida.tseng@hwaotech.com>
2023-07-31 18:15:38 +0800

    考題頁完成

--- src/components/UserLogin.jsx
+++ src/components/UserLogin.jsx
@@ -23,7 +23,7 @@ const UserLogin = () => {
       (res) => {
         sessionStorage.setItem(userLoginKey, userId);
         const selectKey = `01,存款業務,A,存款業務及開戶審查`
-        navigate('/aboutClass')
+        navigate('/classList')
         changeItem(selectKey)
         changeLogin()
       },
